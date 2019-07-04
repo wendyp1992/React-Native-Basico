dc159708a173fcf3006358f9a0bb10de5afc12ca
WENDY ESTUPIÑAN <westupinan@dp-userback8.deltacomunicaciones.com>
2019-07-04 15:03:33 -0500

    agregar estilos a la aplicacion

--- App.js
+++ App.js
@@ -3,12 +3,39 @@ import { StyleSheet, Text, View, Image, AppRegistry } from "react-native";
 
 export default class Bananas extends Component {
   render() {
-    let pic = {
-      uri:
-        "https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg"
-    };
-    return <Image source={pic} style={{ width: 500, height: 300 }} />;
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Hello world!</Text>
+        <Text style={styles.instructions}>
+          La siguiente es una guia para que puedas avanzar en el mundo de React
+          Native
+        </Text>
+        <Text style={styles.instructions}>
+          Presione Cmd+R para recargar, {"\n"}, Cmd+D or shake for dev menu.
+        </Text>
+      </View>
+    );
   }
 }
 
-AppRegistry.registerComponent("AwesomeProject", () => Bananas);
+const styles = StyleSheet.create({
+  //StyleSheet permite declarar los estilos de la aplicacion
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center",
+    backgroundColor: "gray"
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    color: "red",
+    margin: 10
+  },
+  instructions: {
+    textAlign: "center",
+    color: "purple",
+    marginBottom: 5
+  }
+});
+AppRegistry.registerComponent("Agenda Citas", () => Bananas);
