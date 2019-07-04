4da00ff99e106a82181a72ba6c1ec7650c94a937
WENDY ESTUPIÑAN <westupinan@dp-userback8.deltacomunicaciones.com>
2019-07-04 09:58:56 -0500

    agregar imagen en page principal

--- App.js
+++ App.js
@@ -1,19 +1,14 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { Component } from "react";
+import { StyleSheet, Text, View, Image, AppRegistry } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! </Text>
-    </View>
-  );
+export default class Bananas extends Component {
+  render() {
+    let pic = {
+      uri:
+        "https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg"
+    };
+    return <Image source={pic} style={{ width: 500, height: 300 }} />;
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+AppRegistry.registerComponent("AwesomeProject", () => Bananas);
